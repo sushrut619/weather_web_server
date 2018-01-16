f6ba778ff031b8f14a9b5e62379841236830cf3c
sushrut-desora <sushrut@desora.co>
2018-01-16 00:22:20 -0500

    print statements

--- weather_web_app/domain/services/weather_service.py
+++ weather_web_app/domain/services/weather_service.py
@@ -16,17 +16,19 @@ class WeatherService:
         if valid_coordinates:
             api_request_model = self.weather_service_factory.build_dark_sky_api_model(request_model)
             print("fetching weather data from coordinates")
-            
+
         elif request_model.address is not None and len(request_model.address) > 0:
             api_request_model = self.weather_service_factory.get_coordinates_from_address(request_model.address, self.config)
             print("fetching weather data from address")
 
+        print("api request model 1: ", api_request_model)
         weather_data = None
         if api_request_model is not None:
             weather_data = self.api_factory.get_weekly_forecast(api_request_model)
             if request_model.language_flag == True:
                 self.to_fahrenheit(weather_data)
 
+        print("api_request_model2: ", api_request_model)
         print("response: ", weather_data)
         return weather_data
 
