b3e7c58a333b97d5fe6bdb1bdcfb47f03aa92413
sushrut-desora <sushrut@desora.co>
2018-02-02 17:53:41 -0500

    change language header parsing

--- weather_web_app/app/request_model_factories/weather_model_factory.py
+++ weather_web_app/app/request_model_factories/weather_model_factory.py
@@ -8,16 +8,28 @@ class WeatherModelFactory:
         headers = request_dom["headers"]
         query = request_dom["query"]
 
+        language = None
+        if headers['language'] is not None:
+            language = headers['language']
+            language = language.split(',')
+            language = language[0].split(';')
+
         print("language header: ", headers.get('language'))
         return WeatherRequestModel(
             address=query.get("address"),
             latitude=query.get("latitude"),
             longitude=query.get("longitude"),
+            # language_flag = True if (
+            #                          headers['language'] is not None and
+            #                             (
+            #                                 headers['language'] in ["en", "en_US", "en-US", "en-US,en;q=0.9"] or
+            #                                 "en" in re.split(re.split(',|;|=', headers['language']))
+            #                             )
+            #                          ) else False
             language_flag = True if (
-                                     headers['language'] is not None and
+                                        language is not None and
                                         (
-                                            headers['language'] in ["en", "en_US", "en-US", "en-US,en;q=0.9"] or
-                                            "en" in re.split(re.split(',|;|=', headers['language']))
+                                            language[0] in ["en", "en_US", "en-US"]
                                         )
-                                     ) else False
+                                    ) else False
         )
